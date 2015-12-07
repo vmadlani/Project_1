59747f2f37ca6134b1b14974efc70936b83011cd
vmadlani <vivek.madlani@gmail.com>
2015-12-07 12:33:06 +0000

    added displayBoard

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,10 +1,32 @@
 $(document).ready(function(){
 
+  var content = [
+    ["TOPIC","ANSWER 1","ANSWER 2","ANSWER 3","ANSWER 4"  ],
+    ["NAME A CHARACTER FROM THE WIZARD OF OZ","TIN MAN","DOROTHY","LION","SCARECROW"  ],
+    ["NAME SOMETHING THAT HELPS YOU FALL ASLEEP","ALCOHOL","SLEEPING PILLS","WATCHING TV","LISTEN TO MUSIC"  ],
+    ["NAME SOMETHING THAT DRIVERS DO WITH THEIR HANDS WHEN THEY SHOULD BE AT THE WHEEL","USE A MOBILE PHONE","PICK YOUR NOSE","SMOKE","MESSING WITH RADIO/CASSETTE"  ],
+    ["NAME SOMETHING ASSOCIATED WITH HIPPIES","DRUGS","LONG HAIR","FLOWERS(POWER)","FREE LOVE"  ],
+    ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'","TWIX","TOBLERONE","TIME-OUT","TOPIC"  ],
+    ["NAME SOMETHING PEOPLE DO WHEN THEY HAVE FINISHED A MEAL","BURP","LIGHT A CIGARETTE","HAVE COFFEE/TEA","WIPE YOUR MOUTH"  ],
+    ["NAME SOMETHING YOU CAN SEE BUT YOU CANNOT TOUCH","THE SUN","THE MOON","THE CLOUDS","A RAINBOW"  ],
+    ["NAME SOMETHING YOU BUY OR HIRE FOR A WEDDING","WEDDING DRESS","SUIT/TOP HAT & TAILS","CAR","FLOWERS"  ]
+  ]
+
+
+
 var p1Score = 0;
 var p2Score = 0;
-var topicAnswerArray = ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'", "TWIX", "TOBLERONE", "TIME_OUT", "TOPIC"]; 
-var choices = ["TWIX", "TIME_OUT", "TOPIC", "TOBLERONE"];
+var topicAnswerArray = content[5]; // 0, 1, 2, 3, 4
+var choices = ["", "", "", ""];
 var round = 0;
+var correctCells = 0;
+
+// var i = Math.round(Math.random());
+// //Generate random answers in the choices array
+// choices[0]= 
+// choices[1]=
+// choices[2]=
+// choices[3]=
 
 $("#player1Score").text("player 1 score: " + (p1Score));
 $("#player2Score").text("player 2 score: " + (p2Score));
@@ -28,8 +50,38 @@ var key4 = "#" + topicAnswerArray[4]
 console.log(key1 , key2, key3, key4);
 
 $( ".answers" ).draggable({
-    helper : "clone", cursor : "move"
+    helper : "clone", 
+    cursor : "move"
 });
+
+$(key1).draggable({
+  helper : "clone", cursor : "move"
+});
+$(key2).draggable({
+  helper : "clone", cursor : "move"
+});
+$(key3).draggable({
+  helper : "clone", cursor : "move"
+});
+$(key4).draggable({
+  helper : "clone", cursor : "move"
+});
+
+console.log("enabled")
+//$( ".answers" ).draggable( 'disable' );
+//console.log("disabled")
+
+checkWinner = function() {
+  console.log($(key1).text());
+  if (($(key1).text() === "already selected") && ($(key2).text() === "already selected")  && ($(key3).text() === "already selected") && ($(key4).text() === "already selected")){
+    alert("HELLO")
+    reset();
+  };
+}
+
+reset = function() {
+  console.log("RESET")
+}
 
 //TAKEN OUT CHUNK OF CODE REFERENCE "DRAGGABLE QUALITIES"
 
@@ -46,15 +98,26 @@ $( "#droppable1" ).droppable({
     //console.log(event.toElement.innerHTML);
     if (event.toElement.innerHTML === topicAnswerArray[1]) 
       {$( "#droppable1" ).text(topicAnswerArray[1]);
-      if (round%2 === 0) {p1Score++;} else {p2Score++}
-      console.log("correct")}
-    else {console.log("wrong")
+      //console.log(event.toElement.innerHTML);
+      //console.log($("#"+event.toElement.innerHTML));
+      $("#"+event.toElement.innerHTML).text("already selected");
+      //console.log(choices.index[event.toElement.innerHTML])
+      //alert("stop")
+      //console.log($("#"+event.toElement.innerHTML));
+      //var currentBox = $("#"+event.toElement.innerHTML)
+      //currentBox.draggable("disable");
+      //$(""#"" + event.toElement.innerHTML+ """).draggable("disable");
+      //event.toElement.addClass(".done);
+      if (round%2 === 0) {p1Score=p1Score+40;} else {p2Score=p2Score+40}
+      $("#displayBoard").text("Nice one - correct")}
+    else {$("#displayBoard").text("Sorry dude - that's wrong")
     }
     //$( "#droppable1" ).text(topicAnswerArray[1])
     round++;
     $("#player1Score").text("player 1 score: " + (p1Score));
     $("#player2Score").text("player 2 score: " + (p2Score));
     $("#round").text("round: " + (round));
+    checkWinner();
     //$("p1Score").text("HELLO")
     //alert("correct!")
   //$("#round").text("round: " + (round));
@@ -68,15 +131,17 @@ $( "#droppable2" ).droppable({
   hoverClass:  "hover",
    drop: function(event, ui) {    
     if (event.toElement.innerHTML === topicAnswerArray[2]) 
-      {$( "#droppable2" ).text(topicAnswerArray[2])
-      if (round%2 === 0) {p1Score++;} else {p2Score++};
-      console.log("correct")}
-    else {console.log("wrong")
+      {$( "#droppable2" ).text(topicAnswerArray[2]);
+      $("#"+event.toElement.innerHTML).text("already selected");
+      if (round%2 === 0) {p1Score=p1Score+30;} else {p2Score=p2Score+30};
+      $("#displayBoard").text("Nice one - correct")}
+    else {$("#displayBoard").text("Sorry dude - that's wrong")
     }
     round++;
     $("#player1Score").text("player 1 score: " + (p1Score));
     $("#player2Score").text("player 2 score: " + (p2Score));
     $("#round").text("round: " + (round));
+    checkWinner();
   }
  });
 
@@ -87,16 +152,17 @@ $( "#droppable3" ).droppable({
   hoverClass:  "hover",
    drop: function(event, ui) {    
     if (event.toElement.innerHTML === topicAnswerArray[3]) 
-      {$( "#droppable3" ).text(topicAnswerArray[3])
-      if (round%2 === 0) {p1Score++;} else {p2Score++};
-      //alert("correct")
-      console.log("correct")}
-    else { console.log("wrong")
+      {$( "#droppable3" ).text(topicAnswerArray[3]);
+      $("#"+event.toElement.innerHTML).text("already selected");
+      if (round%2 === 0) {p1Score=p1Score+20;} else {p2Score=p2Score+20};
+    $("#displayBoard").text("Nice one - correct")}
+    else {$("#displayBoard").text("Sorry dude - that's wrong")
     }
     round++;
     $("#player1Score").text("player 1 score: " + (p1Score));
     $("#player2Score").text("player 2 score: " + (p2Score));
     $("#round").text("round:" + (round));
+    checkWinner();
   }
  });
 
@@ -107,15 +173,18 @@ $( "#droppable4" ).droppable({
   hoverClass:  "hover",
    drop: function(event, ui) {    
     if (event.toElement.innerHTML === topicAnswerArray[4]) 
-      {$( "#droppable4" ).text(topicAnswerArray[4])
-      if (round%2 === 0) {p1Score++;} else {p2Score++};}
+      {$( "#droppable4" ).text(topicAnswerArray[4]);
+      $("#"+event.toElement.innerHTML).text("already selected");
+      if (round%2 === 0) {p1Score=p1Score+10;} else {p2Score=p2Score+10};
       //alert("correct")
-    else {console.log("wrong")
+      $("#displayBoard").text("Nice one - correct")}
+      else {$("#displayBoard").text("Sorry dude - that's wrong")
     }
     round++;
     $("#player1Score").text("player 1 score: " + (p1Score));
     $("#player2Score").text("player 2 score: " + (p2Score));
     $("#round").text("round:" + (round));
+    checkWinner();
   }
  });
 
