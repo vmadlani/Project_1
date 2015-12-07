82b27cac5ddfdd61e2b32a96d59ab5b4a01d6b9b
vmadlani <vivek.madlani@gmail.com>
2015-12-07 09:43:58 +0000

    added Player 2

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
-var p1Score = 0
-var p2Score = 0
-var topicAnswerArray = ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'", "TWIX", "TOBLERONE", "TIME_OUT", "TOPIC"] 
-var choices = ["TWIX", "TOBLERONE", "TIME_OUT", "TOPIC"]
+var p1Score = 0;
+var p2Score = 0;
+var topicAnswerArray = ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'", "TWIX", "TOBLERONE", "TIME_OUT", "TOPIC"]; 
+var choices = ["TWIX", "TIME_OUT", "TOPIC", "TOBLERONE"];
+var round = 0;
+
+$("#player1Score").text("player 1 score: " + (p1Score));
+$("#player2Score").text("player 2 score: " + (p2Score));
+$("#round").text("round: " + (round));
 
 $("#C1").text(choices[0]);
 $("#C2").text(choices[1]);
@@ -20,30 +25,13 @@ var key2 = "#" + topicAnswerArray[2]
 var key3 = "#" + topicAnswerArray[3]
 var key4 = "#" + topicAnswerArray[4]
 
-console.log(key1);
-console.log(key2);
-console.log(key3);
-console.log(key4);
+console.log(key1 , key2, key3, key4);
 
-$( ".answers" ).draggable();
-$(key1).draggable({
-  helper : "clone",
-  cursor : "move"
-});
-$(key2).draggable({
-  helper : "clone"
-});
-$(key3).draggable({
-  helper : "clone"
-});
-$(key4).draggable({
-  helper : "clone"
+$( ".answers" ).draggable({
+    helper : "clone", cursor : "move"
 });
 
-
-// $( ".draggable" ).draggable();
-//   var choiceID = $(this).val();
-//   console.log(choiceID);
+//TAKEN OUT CHUNK OF CODE REFERENCE "DRAGGABLE QUALITIES"
 
 // THIS WORKSSSSS!!!!!
 $( "#droppable1" ).droppable({
@@ -52,53 +40,82 @@ $( "#droppable1" ).droppable({
   //activeClass: "active",
   hoverClass:  "hover",
    drop: function(event, ui) {  	
-    console.log(event);
-    console.log(event.target.innerHTML);
-    console.log(topicAnswerArray[1]);
-    console.log(event.toElement.innerHTML);
+    //console.log(event);
+    //console.log(event.target.innerHTML);
+    //console.log(topicAnswerArray[1]);
+    //console.log(event.toElement.innerHTML);
     if (event.toElement.innerHTML === topicAnswerArray[1]) 
-      {alert("hallejulah!!!")}
-    else {alert("wrong")}
+      {$( "#droppable1" ).text(topicAnswerArray[1]);
+      if (round%2 === 0) {p1Score++;} else {p2Score++}
+      console.log("correct")}
+    else {console.log("wrong")
+    }
     //$( "#droppable1" ).text(topicAnswerArray[1])
-    p1Score++;
+    round++;
+    $("#player1Score").text("player 1 score: " + (p1Score));
+    $("#player2Score").text("player 2 score: " + (p2Score));
+    $("#round").text("round: " + (round));
+    //$("p1Score").text("HELLO")
     //alert("correct!")
+  //$("#round").text("round: " + (round));
   }     
-     
  });
 
 $( "#droppable2" ).droppable({
   tolerance : "intersect",
-  accept : key2,
-  activeClass: "active",
+  //accept : key2,
+  //activeClass: "active",
   hoverClass:  "hover",
    drop: function(event, ui) {    
-    $( "#droppable2" ).text(topicAnswerArray[2])
-    p1Score++;
-    alert("correct!")
+    if (event.toElement.innerHTML === topicAnswerArray[2]) 
+      {$( "#droppable2" ).text(topicAnswerArray[2])
+      if (round%2 === 0) {p1Score++;} else {p2Score++};
+      console.log("correct")}
+    else {console.log("wrong")
+    }
+    round++;
+    $("#player1Score").text("player 1 score: " + (p1Score));
+    $("#player2Score").text("player 2 score: " + (p2Score));
+    $("#round").text("round: " + (round));
   }
  });
 
 $( "#droppable3" ).droppable({
   tolerance : "intersect",
-  accept : key3,
-  activeClass: "active",
+  //accept : key3,
+  //activeClass: "active",
   hoverClass:  "hover",
    drop: function(event, ui) {    
-    $( "#droppable3" ).text(topicAnswerArray[3])
-    p1Score++;
-    alert("correct!")
+    if (event.toElement.innerHTML === topicAnswerArray[3]) 
+      {$( "#droppable3" ).text(topicAnswerArray[3])
+      if (round%2 === 0) {p1Score++;} else {p2Score++};
+      //alert("correct")
+      console.log("correct")}
+    else { console.log("wrong")
+    }
+    round++;
+    $("#player1Score").text("player 1 score: " + (p1Score));
+    $("#player2Score").text("player 2 score: " + (p2Score));
+    $("#round").text("round:" + (round));
   }
  });
 
 $( "#droppable4" ).droppable({
   tolerance : "intersect",
-  accept : key4,
-  activeClass: "active",
+  //accept : key4,
+  //activeClass: "active",
   hoverClass:  "hover",
    drop: function(event, ui) {    
-    $( "#droppable4" ).text(topicAnswerArray[4])
-    p1Score++;
-    alert("correct!")
+    if (event.toElement.innerHTML === topicAnswerArray[4]) 
+      {$( "#droppable4" ).text(topicAnswerArray[4])
+      if (round%2 === 0) {p1Score++;} else {p2Score++};}
+      //alert("correct")
+    else {console.log("wrong")
+    }
+    round++;
+    $("#player1Score").text("player 1 score: " + (p1Score));
+    $("#player2Score").text("player 2 score: " + (p2Score));
+    $("#round").text("round:" + (round));
   }
  });
 
@@ -107,3 +124,17 @@ $( "#droppable4" ).droppable({
 
 });
 // twix, toblerone, time-out, topic
+
+// REFERENCE: DRAGGABLE QUALITIES
+// $(key1).draggable({
+//   helper : "clone", cursor : "move"
+// });
+// $(key2).draggable({
+//   helper : "clone", cursor : "move"
+// });
+// $(key3).draggable({
+//   helper : "clone", cursor : "move"
+// });
+// $(key4).draggable({
+//   helper : "clone", cursor : "move"
+// });
